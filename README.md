d26f5ddfa3140ea2a4512095eabf9a6fa5ba5695
acemaster <vivekhtc25@gmail.com>
2015-12-08 22:27:42 +0530

    finished script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AceScripts
 Some automation Scripts for the betterment of my life :D
+
+
+## iamcoming.sh
+
+This script is for quick changing of proxy servers on github and on the terminal. I travel from Kuwait to India every six months so this is for quick changing
+1. chmod +x iamcoming.sh
+2. . iamcoming.sh locale This locale is either college or kuwait
+```
+. iamcoming.sh college
+. iamcoming.sh kuwait
+
+```
+
+
--- iamcoming.sh
+++ iamcoming.sh
@@ -1,16 +1,19 @@
 echo You are changing your locale to $1
 
 changelocale(){
-	echo Changing proxy into $1
-
+	export http_proxy=$1
+	git config --global http.proxy $1
+	git config --global https.proxy $1
 }
 
-proxy = http\:\/\/172.30.0.7\:3128
+proxy="http://172.30.0.7:3128"
 if [[ $1 = college ]]; then
 	echo Changing proxy to $proxy
+	changelocale $proxy
 else
 	proxy=""
-	echo Outside college....No proxy needed 
+	echo Outside college....No proxy needed
+	unset http_proxy
+	git config --global --unset http.proxy
+	git config --global --unset https.proxy
 fi
-
-changelocale $proxy
